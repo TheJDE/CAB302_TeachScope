d3085995eb40236f791cd8ebb359baed4395dc28
Dex Hewitt <dexhewitt@outlook.com>
2025-09-14 11:36:51 +1000

    Clean up unused imports.

--- src/main/java/com/cab302/teachscope/controllers/SignupController.java
+++ src/main/java/com/cab302/teachscope/controllers/SignupController.java
@@ -1,17 +1,13 @@
 package com.cab302.teachscope.controllers;
 
-import com.cab302.teachscope.Main;
 import com.cab302.teachscope.util.NavigationUtils;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.kordamp.bootstrapfx.BootstrapFX;
 
 import com.cab302.teachscope.models.dao.DbUserDao;
 import com.cab302.teachscope.models.services.UserService;
