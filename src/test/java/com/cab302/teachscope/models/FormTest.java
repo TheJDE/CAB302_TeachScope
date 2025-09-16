2704825f6b3bcb00aeb0eb4d5de235d396481420
TheJDE <jdeiseman@hotmail.com>
2025-09-16 11:36:47 +1000

    Add initial test cases to FormTest

---
+++ src/test/java/com/cab302/teachscope/models/FormTest.java
@@ -0,0 +1,77 @@
+package com.cab302.teachscope.models;
+
+
+import com.cab302.teachscope.models.dao.MockFormDao;
+import com.cab302.teachscope.models.dao.FormDao;
+import com.cab302.teachscope.models.dao.MockUserDao;
+import com.cab302.teachscope.models.entities.WeeklyForm;
+import com.cab302.teachscope.models.services.FormService;
+import com.cab302.teachscope.models.services.UserService;
+import com.cab302.teachscope.util.PasswordUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class FormTest {
+    private FormDao formdao;
+    private FormService formService;
+    private int StudentId;
+    private int term;
+    private int week;
+    private int attenanceDays;
+    private boolean wasLate;
+    private boolean homeworkDone;
+    private int attentionScore;
+    private int literacyScore;
+    private int numeracyScore;
+    private int understandingScore;
+    private int behaviourScore;
+    private String createdAt;
+
+    private
+    @BeforeEach
+    void setup() {
+        formdao = new MockFormDao();
+        formService = new FormService(formdao);
+        StudentId = 1;
+        term = 1;
+        week = 1;
+        attenanceDays = 1;
+        wasLate = false;
+        homeworkDone = true;
+        attentionScore = 1;
+        literacyScore = 1;
+        numeracyScore = 1;
+        understandingScore = 1;
+        behaviourScore = 1;
+        createdAt = "Week 1, Term 1";
+
+
+
+    }
+
+    @Test
+    void createFormValid() {
+
+    }
+
+    @Test
+    void createFormInvalidTerm() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            formService.createForm(
+                    new WeeklyForm(StudentId, term, week, attenanceDays, wasLate, homeworkDone, attentionScore, literacyScore, numeracyScore, understandingScore, behaviourScore, createdAt)
+
+            );
+        });
+    }
+
+    @Test
+    void createFormNullTerm() {
+
+    }
+
+    @Test
+    void createFormInvalid_field2() {
+    }
+}
