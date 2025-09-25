351505e296e4ec95bf659d47e6e3c00ef29d8b2b
Dex Hewitt <dexhewitt@outlook.com>
2025-09-25 19:49:00 +1000

    Add database connection test.

--- src/test/java/com/cab302/teachscope/models/DatabaseTest.java
+++ src/test/java/com/cab302/teachscope/models/DatabaseTest.java
@@ -1,4 +1,15 @@
 package com.cab302.teachscope.models;
 
+import com.cab302.teachscope.DatabaseConnection;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DatabaseTest {
+    @Test
+    public void testConnection() {
+        Connection conn = DatabaseConnection.getInstance();
+        assertNotNull(conn);
+    }
 }
