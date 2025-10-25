58fba9921180df3b7dfca8fd042c043545dcb480
Prapti <praptiadhikari52@gmail.com>
2025-10-25 14:09:44 +1000

    Add invalid week test for report generation.

--- src/test/java/com/cab302/teachscope/models/FormTest.java
+++ src/test/java/com/cab302/teachscope/models/FormTest.java
@@ -903,4 +903,48 @@ public class FormTest {
         assertThrows(IllegalArgumentException.class,
                 () -> generateReportsService.createReport(StudentId, 0, 1, 10));
     }
+
+    @Test
+    void studentAveragesInvalidWeek() {
+        formService.createForm(
+                StudentId,
+                term,
+                week,
+                attendanceDays,
+                daysLate,
+                attentionScore,
+                homeworkDone,
+                participationScore,
+                literacyScore,
+                numeracyScore,
+                understandingScore,
+                behaviourScore,
+                peerInteractionScore,
+                respectForRulesScore,
+                emotionalState,
+                teacherConcerns
+        );
+
+        formService.createForm(
+                StudentId,
+                term,
+                week + 1,
+                attendanceDays,
+                daysLate,
+                attentionScore,
+                homeworkDone,
+                participationScore,
+                literacyScore,
+                numeracyScore,
+                understandingScore,
+                behaviourScore,
+                peerInteractionScore,
+                respectForRulesScore,
+                emotionalState,
+                teacherConcerns
+        );
+
+        assertThrows(IllegalArgumentException.class,
+                () -> generateReportsService.createReport(StudentId, 1, 1, 12));
+    }
 }
